9f973d60a58500e4211171939fd136f54a5546eb
Nikos Kokkos <nkokkos@gmail.com>
2013-07-29 16:14:02 +0300

    added initialize and timestamp methods

--- lib/vws.rb
+++ lib/vws.rb
@@ -1,5 +1,30 @@
 require "vws/version"
+require 'rubygems'
+require 'hmac-sha1'
+require 'rest_client'
+
+
+
 
 module Vws
-  # Your code goes here...
+  
+  #constants for end point interface links
+  #
+  #
+  #
+  class Api
+
+    def initialize(key=nil, secret=nil)
+      @key     = key || ENV['VWS_KEY']
+      @secrete = key || ENV['VWS_SECRET']
+    end
+
+
+    def timestamp
+      return Time.now.utc.strftime("%Y%m%d%H%M%S")
+    end
+
+
+  end
+
 end
