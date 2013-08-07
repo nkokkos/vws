e23cc35c58ffc5c959aa25507883d12f3fa77010
Nikos Kokkos <nkokkos@gmail.com>
2013-08-07 16:29:45 +0300

    added more methods

--- lib/vws.rb
+++ lib/vws.rb
@@ -3,15 +3,11 @@ require 'rubygems'
 require 'hmac-sha1'
 require 'rest_client'
 
-
-
-
 module Vws
   
   #constants for end point interface links
-  #
-  #
-  #
+  BASE_URL = "https://vws.examplecompany.com/targets"
+  
   class Api
 
     def initialize(key=nil, secret=nil)
@@ -19,7 +15,12 @@ module Vws
       @secrete = key || ENV['VWS_SECRET']
     end
 
-
+	def build_signature
+	end
+	
+	def signed_request	
+	end
+	
     def timestamp
       return Time.now.utc.strftime("%Y%m%d%H%M%S")
     end
