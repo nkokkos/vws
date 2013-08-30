bce0c81ae7895f914a240b077bbae7bbb3923584
Nikos Kokkos <nkokkos@gmail.com>
2013-08-30 16:08:00 +0300

    ditched ruby-hmac in favor of openssl HMAC methods. More changed to build signature

--- lib/vws.rb
+++ lib/vws.rb
@@ -4,6 +4,8 @@ require 'hmac-sha1'
 require 'rest_client'
 require 'digest/md5'
 require 'time'
+require 'openssl'
+require 'base64'
 
 module Vws
   
@@ -13,18 +15,18 @@ module Vws
   
   class Api
 
-    def initialize(key=nil, secret=nil)
-      @key     = key || ENV['VWS_KEY']
-      @secret  = secret || ENV['VWS_SECRET']
+    def initialize(accesskey=nil, secretkey=nil)
+      @accesskey = accesskey ||  ENV['VWS_ACCESSKEY']
+      @secretkey = secretkey || ENV['VWS_SECRETKEY']
     end
 
-	def build_signature(full_path, request, secret_key)
+	def build_signature(full_path, request_method, secret_key)
       contentType = "";
 	  hexDigest = "d41d8cd98f00b204e9800998ecf8427e" # Hex digest of an empty string	  
 	  
-	  if request == "GET" || request == "DELETE" 
-	    #Do nothing
-	  elsif request == "POST" || request === "PUT" 
+	  if request_method == "GET" || request_method == "DELETE" 
+	    #Do nothing since we have already set contentType and hexDigest
+	  elsif request_method == "POST" || request_method === "PUT" 
 	    contentType = "application/json";
 		#the request should have a request body
 		hexDigest = Digest::MD5.hexdigest(full_path)
@@ -32,8 +34,14 @@ module Vws
 		  puts "Invalid content type";
 	   end
 	   
-	  #rawstr = ""
-      #hmac  = HMAC::SHA1.new(@secret)	   
+	   dateValue = "" #get value from header (implement this later on)
+       toDigest  = request_method + "\n" + hexDigest + "\n" + 
+				   contentType + "\n" + dateValue + "\n" + full_path
+
+	   signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), @secretkey, toDigest)
+ 
+       signature_toBase64 = Base64.encode64(signature)
+ 
 	end
 	
 	def signed_request(url, method)
