887eed15ab2192719eecc58acf9ab62802091605
Nikos Kokkos <nkokkos@gmail.com>
2013-09-02 16:22:33 +0300

    more changed to methods

--- lib/vws.rb
+++ lib/vws.rb
@@ -1,6 +1,5 @@
 require "vws/version"
 require 'rubygems'
-require 'hmac-sha1'
 require 'rest_client'
 require 'digest/md5'
 require 'time'
@@ -10,49 +9,52 @@ require 'base64'
 module Vws
   
   #constants for end point interface links
-  BASE_URL		= "https://vws.examplecompany.com/"
-  TARGET_URL	= "https://vws.examplecompany.com/targets"
+  BASE_URL = "http://requestb.in/saztgxsb"
+  #BASE_URL		= "https://vws.examplecompany.com/"
+  TARGET_URL	= BASE_URL + "targets"
   
   class Api
 
     def initialize(accesskey=nil, secretkey=nil)
-      @accesskey = accesskey ||  ENV['VWS_ACCESSKEY']
-      @secretkey = secretkey || ENV['VWS_SECRETKEY']
+	  @accesskey = accesskey ||  ENV['VWS_ACCESSKEY']
+	  @secretkey = secretkey || ENV['VWS_SECRETKEY']
     end
 
-	def build_signature(full_path, request_method, secret_key)
-      contentType = "";
+	def build_signature(full_path, request_method, timestamp)
+	  contentType = ""
 	  hexDigest = "d41d8cd98f00b204e9800998ecf8427e" # Hex digest of an empty string	  
 	  
 	  if request_method == "GET" || request_method == "DELETE" 
 	    #Do nothing since we have already set contentType and hexDigest
 	  elsif request_method == "POST" || request_method === "PUT" 
 	    contentType = "application/json";
-		#the request should have a request body
-		hexDigest = Digest::MD5.hexdigest(full_path)
+	    #the request should have a request body
+	    hexDigest = Digest::MD5.hexdigest(full_path)
 		else 
-		  puts "Invalid content type";
-	   end
+		  puts "Invalid request method";
+		  return nil
+	  end
 	   
-	   dateValue = "" #get value from header (implement this later on)
-       toDigest  = request_method + "\n" + hexDigest + "\n" + 
+	  dateValue = timestamp
+      toDigest  = request_method + "\n" + hexDigest + "\n" + 
 				   contentType + "\n" + dateValue + "\n" + full_path
-
-	   signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), @secretkey, toDigest)
- 
-       signature_toBase64 = Base64.encode64(signature)
- 
-	end
-	
-	def signed_request(url, method)
-	
+	 #generate hmac digest with openssl methods	 
+	 signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), @secretkey, toDigest)
+     #encode result to base64 
+	 signature_toBase64 = Base64.encode64(signature)
 	end
 	
     def timestamp
 	  #Date is the current date per RFC 2616, section 3.3.1, rfc1123-date format, e.g.: Sun, 22 Apr #2012 08:49:37 GMT.
       return Time.now.httpdate
     end
-
+	
+	def list_targets
+	  date_timestamp = self.timestamp
+	  authorization_header = "Authorization: VWS " + @accesskey + ":" +  build_signature(TARGET_URL, 'GET', date_timestamp)
+	  RestClient.get(TARGET_URL, :'Date' => date_timestamp, :'Authorization' => authorization_header, :content_type => :json)
+	end
+	
   end
 
 end
