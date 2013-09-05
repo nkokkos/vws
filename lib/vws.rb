29457c932270e264d3b43c7b35c23a8b4179a5a7
Nick Kokkos <nkokkos@gmail.com>
2013-09-05 12:20:47 +0300

    fixed signature problems. Successful connection to webservice. At this point  is only works for empty get requests

--- lib/vws.rb
+++ lib/vws.rb
@@ -8,54 +8,45 @@ require 'base64'
 
 module Vws
   #constants for end point interface links
-  BASE_URL = "http://requestb.in/1bm5ihn1" # use this for testing api. Will remove/replace at the end
-  #BASE_URL = "https://vws.examplecompany.com/"
-  TARGET_URL = BASE_URL + "targets"
+  BASE_URL = "https://vws.vuforia.com/"
   
   class Api
   
     def initialize(accesskey=nil, secretkey=nil)
-      accesskey = accesskey ||  ENV['VWS_ACCESSKEY']
-      @secretkey = secretkey || ENV['VWS_SECRETKEY']
+      @accesskey = accesskey ||  ENV['VWS_ACCESSKEY']
+      @secretkey = secretkey ||  ENV['VWS_SECRETKEY']
 	end
 
-    def build_signature(full_path, request_method, timestamp)
+    def build_signature(request_path, http_verb, timestamp)
       contentType = ""
       hexDigest = "d41d8cd98f00b204e9800998ecf8427e" # Hex digest of an empty string	  
 	  
-      if request_method == "GET" || request_method == "DELETE" 
+      if http_verb == "GET" || http_verb == "DELETE" 
 	    #Do nothing since we have already set contentType and hexDigest
-      elsif request_method == "POST" || request_method === "PUT" 
+      elsif http_verb == "POST" || http_verb === "PUT" 
         contentType = "application/json";
         #the request should have a request body
-        hexDigest = Digest::MD5.hexdigest(full_path)
+        hexDigest = Digest::MD5.hexdigest(request_path)
       else 
         puts "Invalid request method";
         return nil
       end
 
-      dateValue = timestamp
-      toDigest  = request_method + "\n" + hexDigest + "\n" + 
-				   contentType + "\n" + dateValue + "\n" + full_path
-	 
-      #generate hmac digest with openssl methods	 
-      signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), @secretkey, toDigest)
-      
-      #encode result to base64 
-      signature_toBase64 = Base64.encode64(signature)
-    
+      toDigest  = http_verb + "\n" + hexDigest + "\n" + contentType + "\n" + timestamp + "\n" + request_path
+      return Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1.new, @secretkey, toDigest))  
     end
 	
-    def timestamp
-      #Date is the current date per RFC 2616, section 3.3.1, rfc1123-date format, e.g.: Sun, 22 Apr #2012 08:49:37 GMT.
-      return Time.now.httpdate
-    end
-	
+    # Calls the api end point for the list of targets associated with access key and database
     def list_targets
-      date_timestamp = self.timestamp
-      authorization_header = "Authorization: VWS " + @accesskey + ":" +  self.build_signature(TARGET_URL, 'GET', date_timestamp)
-      RestClient.get(BASE_URL, :'Date' => date_timestamp, :'Authorization' => authorization_header, :content_type => :json)
-	end
+	  #Date is the current date per RFC 2616, section 3.3.1, rfc1123-date format, e.g.: Sun, 22 Apr #2012 08:49:37 GMT.
+      date_timestamp = Time.now.httpdate
+      authorization_header = "VWS " + @accesskey + ":" +  self.build_signature('/targets', 'GET', date_timestamp)
+	  begin
+	    RestClient.get(BASE_URL + '/targets', :'Date' => date_timestamp, :'Authorization' => authorization_header)
+	   rescue => e
+	    e.response
+	   end
+    end
 	
   end
 
--- spec/vws_spec.rb
+++ spec/vws_spec.rb
@@ -1,8 +1,11 @@
-require 'vws'
+require_relative '../lib/vws.rb'
 
 describe Vws do
-  describe "do nothing" do
-    puts "nothing"
-  end
+  describe "should connect to webservice and fail otherwise" do
+    conn = Vws::Api.new
+	puts conn.inspect
+	response = conn.list_targets
+	puts response
+	end
 end
 
