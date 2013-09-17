6044b18c1e2c299fcbec73db715669e1d944ac2e
Nick Kokkos <nkokkos@gmail.com>
2013-09-17 23:45:48 +0300

    commit retrieve_target method again. Replaced it before by accident

--- lib/vws.rb
+++ lib/vws.rb
@@ -98,8 +98,22 @@ module Vws
       begin
         RestClient.get(SUMMARY_URL, :'Date' => date_timestamp, 
                                     :'Authorization' => authorization_header)
-        rescue => e
-          e.response
+      rescue => e
+        e.response
+      end
+    end
+    
+    def retrieve_target(target_id)
+      date_timestamp = Time.now.httpdate
+      target_id_url = TARGETS_URL + '/' + target_id
+      target_id_suburl = '/targets' + '/' + target_id
+      signature = self.build_signature(target_id_suburl, nil, 'GET', date_timestamp)
+      authorization_header = "VWS " + @accesskey + ":" + signature
+      begin
+        RestClient.get(target_id_url, :'Date' => date_timestamp,
+                                      :'Authorization' => authorization_header)
+      rescue => e
+        e.response
       end
     end
 
