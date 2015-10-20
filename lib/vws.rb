22017b1a4e522477624b28a11f216a80694fc7ee
Fabio 'c0m3tx' Vitale <c0m3tx@gmail.com>
2015-10-20 12:35:53 +0200

    Fixed updating metadata

--- lib/vws.rb
+++ lib/vws.rb
@@ -100,23 +100,18 @@ module Vws
       end
     end
 
-    # Updates an existing target. Target_id must already exist
-    # https://developer.vuforia.com/library/articles/Solution/How-To-Update-a-Target-Using-the-VWS-API
-    def update_target(target_id, target_name=nil, file_path=nil, width=nil, active_flag=nil,application_metadata=nil)
-      raise "target id should not be nil" if target_id == nil
+    def update_target(target_id, target_name=nil, file_path=nil, width=nil, active_flag=nil, metadata=nil)
       date_timestamp = Time.now.httpdate
       target_id_url = TARGETS_URL + '/' + target_id
       target_id_suburl = '/targets' + '/' + target_id
       #for file uploads, read file contents data and Base 64 encode it:
-      contents_encoded = Base64.encode64(File.open(file_path, 'rb').read)
-      application_metadata_encoded = Base64.encode64(application_metadata.to_s)
-      body_hash = {
-		    :name => target_name,
+      contents_encoded = Base64.encode64(open(file_path) { |io| io.read })
+      metadata_encoded = Base64.encode64(metadata.to_s)
+      body_hash = { :name => target_name, 
                     :width => width, #Width of the target in scene unit
                     :image => contents_encoded,
                     :active_flag => active_flag,
-                    :application_metadata => application_metadata_encoded
-  		  }
+                    :application_metadata => metadata_encoded }
       signature = self.build_signature(target_id_suburl, body_hash, 'PUT', date_timestamp)
       raise ArgumentError.new('Signature returned nil. Aborting...') if signature == nil
       authorization_header = "VWS " + @accesskey + ":" +  signature
